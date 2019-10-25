9ef310001912e0b1dd0cf6db7b5e0b74a4fd4184
xiangzepro <34424368@qq.com>
2019-10-26 00:43:53 +0800

    更新 'springbooto2o/src/main/java/com/imooc/o2o/util/ShortNetAddressUtil.java'
    新增短链接生成V2，以应对当前短链接没办法使用的问题

--- springbooto2o/src/main/java/com/imooc/o2o/util/ShortNetAddressUtil.java
+++ springbooto2o/src/main/java/com/imooc/o2o/util/ShortNetAddressUtil.java
@@ -1,129 +1,132 @@
 package com.imooc.o2o.util;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLEncoder;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.google.gson.Gson;
+import com.google.gson.annotations.SerializedName;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
+//需要在pom.xml引入以下依赖
+/**
+ <dependency>
+<groupId>com.google.code.gson</groupId>
+<artifactId>gson</artifactId>
+<version>2.8.6</version>
+</dependency>
+**/
 public class ShortNetAddressUtil {
-	private static Logger log = LoggerFactory.getLogger(ShortNetAddressUtil.class);
+	   final static String CREATE_API = "https://dwz.cn/admin/v2/create";
+	    // 设置Token，在https://dwz.cn/console/userinfo 获取自己的token，复制粘贴到这里
+	    final static String TOKEN = "fd26165fc99cb2208e06a7f1d0dcd4bf"; 
 
-	public static int TIMEOUT = 30 * 1000;
-	public static String ENCODING = "UTF-8";
+	    class UrlResponse {
+	        @SerializedName("Code")
+	        private int code;
 
-	/**
-	 * 根据传入的url，通过访问百度短链接的接口， 将其转换成短的URL
-	 * 
-	 * @param originURL
-	 * @return
-	 */
-	public static String getShortURL(String originURL) {
-		String tinyUrl = null;
-		try {
-			// 指定百度短链接的接口
-			URL url = new URL("http://dwz.cn/create.php");
-			// 建立连接
-			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-			// 设置连接的参数
-			// 使用连接进行输出
-			connection.setDoOutput(true);
-			// 使用连接进行输入
-			connection.setDoInput(true);
-			// 不使用缓存
-			connection.setUseCaches(false);
-			// 设置连接超时时间为30秒
-			connection.setConnectTimeout(TIMEOUT);
-			// 设置请求模式为POST
-			connection.setRequestMethod("POST");
-			// 设置POST信息，这里为传入的原始URL
-			String postData = URLEncoder.encode(originURL.toString(), "utf-8");
-			// 输出原始的url
-			connection.getOutputStream().write(("url=" + postData).getBytes());
-			// 连接百度短视频接口
-			connection.connect();
-			// 获取返回的字符串
-			String responseStr = getResponseStr(connection);
-			log.info("response string: " + responseStr);
-			// 在字符串里获取tinyurl，即短链接
-			tinyUrl = getValueByKey(responseStr, "tinyurl");
-			log.info("tinyurl: " + tinyUrl);
-			// 关闭链接
-			connection.disconnect();
-		} catch (IOException e) {
-			log.error("getshortURL error:" + e.toString());
-		}
-		return tinyUrl;
-	}
+	        @SerializedName("ErrMsg")
+	        private String errMsg;
 
-	/**
-	 * 通过HttpConnection 获取返回的字符串
-	 * 
-	 * @param connection
-	 * @return
-	 * @throws IOException
-	 */
-	private static String getResponseStr(HttpURLConnection connection) throws IOException {
-		StringBuffer result = new StringBuffer();
-		// 从连接中获取http状态码
-		int responseCode = connection.getResponseCode();
+	        @SerializedName("LongUrl")
+	        private String longUrl;
 
-		if (responseCode == HttpURLConnection.HTTP_OK) {
-			// 如果返回的状态码是OK的，那么取出连接的输入流
-			InputStream in = connection.getInputStream();
-			BufferedReader reader = new BufferedReader(new InputStreamReader(in, ENCODING));
-			String inputLine = "";
-			while ((inputLine = reader.readLine()) != null) {
-				// 将消息逐行读入结果中
-				result.append(inputLine);
-			}
-		}
-		// 将结果转换成String并返回
-		return String.valueOf(result);
-	}
+	        @SerializedName("ShortUrl")
+	        private String shortUrl;
 
-	/**
-	 * JSON 依据传入的key获取value
-	 * 
-	 * @param replyText
-	 * @param key
-	 * @return
-	 */
-	private static String getValueByKey(String replyText, String key) {
-		ObjectMapper mapper = new ObjectMapper();
-		// 定义json节点
-		JsonNode node;
-		String targetValue = null;
-		try {
-			// 把调用返回的消息串转换成json对象
-			node = mapper.readTree(replyText);
-			// 依据key从json对象里获取对应的值
-			targetValue = node.get(key).asText();
-		} catch (JsonProcessingException e) {
-			log.error("getValueByKey error:" + e.toString());
-			e.printStackTrace();
-		} catch (IOException e) {
-			log.error("getValueByKey error:" + e.toString());
-		}
+	        public int getCode() {
+	            return code;
+	        }
 
-		return targetValue;
-	}
+	        public void setCode(int code) {
+	            this.code = code;
+	        }
 
-	/**
-	 *  百度短链接接口
-	 * 
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		getShortURL("https://mp.weixin.qq.com/debug/cgi-bin/sandbox?t=sandbox/login");
-	}
+	        public String getErrMsg() {
+	            return errMsg;
+	        }
+
+	        public void setErrMsg(String errMsg) {
+	            this.errMsg = errMsg;
+	        }
+
+	        public String getLongUrl() {
+	            return longUrl;
+	        }
+
+	        public void setLongUrl(String longUrl) {
+	            this.longUrl = longUrl;
+	        }
+
+	        public String getShortUrl() {
+	            return shortUrl;
+	        }
+
+	        public void setShortUrl(String shortUrl) {
+	            this.shortUrl = shortUrl;
+	        }
+	    }
+
+	    /**
+	     * 创建短网址
+	     *
+	     * @param longUrl
+	     *            长网址：即原网址
+	     * @return  成功：短网址
+	     *          失败：返回空字符串
+	     */
+	    public static String getShortURL(String longUrl) {
+	        String params = "{\"Url\":\""+ longUrl + "\"}";
+
+	        BufferedReader reader = null;
+	        try {
+	            // 创建连接
+	            URL url = new URL(CREATE_API);
+	            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+	            connection.setDoOutput(true);
+	            connection.setDoInput(true);
+	            connection.setUseCaches(false);
+	            connection.setInstanceFollowRedirects(true);
+	            connection.setRequestMethod("POST"); // 设置请求方式
+	            connection.setRequestProperty("Content-Type", "application/json"); // 设置发送数据的格式
+	            connection.setRequestProperty("Token", TOKEN); // 设置发送数据的格式");
+
+	            // 发起请求
+	            connection.connect();
+	            OutputStreamWriter out = new OutputStreamWriter(connection.getOutputStream(), "UTF-8"); // utf-8编码
+	            out.append(params);
+	            out.flush();
+	            out.close();
+
+	            // 读取响应
+	            reader = new BufferedReader(new InputStreamReader(connection.getInputStream(), "UTF-8"));
+	            String line;
+	            String res = "";
+	            while ((line = reader.readLine()) != null) {
+	                res += line;
+	            }
+	            reader.close();
+
+	            // 抽取生成短网址
+	            UrlResponse urlResponse = new Gson().fromJson(res, UrlResponse.class);
+	            if (urlResponse.getCode() == 0) {
+	                return urlResponse.getShortUrl();
+	            } else {
+	                System.out.println(urlResponse.getErrMsg());
+	            }
+
+	            return ""; // TODO：自定义错误信息
+	        } catch (IOException e) {
+	            // TODO
+	            e.printStackTrace();
+	        }
+	        return ""; // TODO：自定义错误信息
+	    }
+
+	    public static void main(String[] args) {
+	        String res = getShortURL("http://myo2o.yitiaojieinfo.com/myo2o/frontend/index");
+	        System.out.println(res);
+	    }
 }
